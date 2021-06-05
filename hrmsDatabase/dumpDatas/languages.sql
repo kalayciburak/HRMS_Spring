aa66b9dad5a6a5b7d7df3da253d5afe4169940cf
Burak KALAYCI <torukobyte@gmail.com>
2021-06-05 20:57:35 +0300

    dumpDatas folder added!

---
+++ hrmsDatabase/dumpDatas/languages.sql
@@ -0,0 +1,11 @@
+INSERT INTO languages
+    OVERRIDING SYSTEM VALUE
+VALUES (1, 'İngilizce'),
+       (2, 'Çince'),
+       (3, 'Hintçe'),
+       (4, 'Portekizce'),
+       (5, 'Almanca'),
+       (6, 'Rusça'),
+       (7, 'İtalyanca'),
+       (8, 'Fransızca'),
+       (9, 'Arapça');
